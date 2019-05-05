9df6c5484740f098700772f517009b73c0cf6982
Duy Truong <duytruongh@gmail.com>
2019-05-04 21:04:11 -0700

    Updates

---
+++ precache-manifest.b3d46b9c7e1265f42a2404d6872df3d2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "967006d52480a597d47a5ed7ba1f6656",
+    "url": "/phot_blog/index.html"
+  },
+  {
+    "revision": "1d3c20f4932f15163aa4",
+    "url": "/phot_blog/static/css/main.1ca71a94.chunk.css"
+  },
+  {
+    "revision": "0e8519403942dc2581ff",
+    "url": "/phot_blog/static/js/2.430a6e8f.chunk.js"
+  },
+  {
+    "revision": "1d3c20f4932f15163aa4",
+    "url": "/phot_blog/static/js/main.b45dcfc6.chunk.js"
+  },
+  {
+    "revision": "94a807923474aba341cb",
+    "url": "/phot_blog/static/js/runtime~main.12bc6a66.js"
+  }
+]);
